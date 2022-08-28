a4071644378668c63b70971d4bf8f7254069a208
dsc52 <dsc52@ADMINISTRATOR>
2022-08-28 16:38:44 +0800

    添加项目文件。

---
+++ IspExample29_1/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+Driver driver = new Driver(new HeavyTank());
+driver.Drive();
+
+class Driver
+{
+    private IVehicle _Vehicle;
+    public Driver(IVehicle vehicle)
+    {
+        _Vehicle = vehicle;
+    }
+    public void Drive()
+    {
+        _Vehicle.Run();
+    }
+}
+
+interface IVehicle
+{
+    void Run();
+}
+
+class Car : IVehicle
+{
+    public void Run()
+    {
+        Console.WriteLine("Car is running ...");
+    }
+}
+
+class Truck : IVehicle
+{
+    public void Run()
+    {
+        Console.WriteLine("Truck is running ...");
+    }
+}
+
+interface IWeapon
+{
+    void Fire();
+}
+interface ITank:IVehicle, IWeapon
+{
+
+}
+
+class LightTank : IVehicle
+{
+    public void Fire()
+    {
+        Console.WriteLine("Boom!");
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("Ka Ka Ka");
+    }
+}
+
+class MediumTank : IVehicle
+{
+    public void Fire()
+    {
+        Console.WriteLine("Boom!!");
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("Ka! Ka! Ka!");
+    }
+}
+
+class HeavyTank : ITank
+{
+    public void Fire()
+    {
+        Console.WriteLine("Boom!!!");
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("Ka!! Ka!! Ka!!");
+    }
+}
